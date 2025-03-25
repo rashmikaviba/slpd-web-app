10260a9f183ad3e456dc05ffdf673003854eef2d
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2025-03-25 22:37:00 +0530

    add nes=w expense category

--- src/app/shared/data/expensesCategory.ts
+++ src/app/shared/data/expensesCategory.ts
@@ -8,6 +8,30 @@ export const expenseCategory = [
     name: "Food",
   },
   {
+    id: 4,
+    name: "Accommodation",
+  },
+  {
+    id: 5,
+    name: "Express way",
+  },
+  {
+    id: 6,
+    name: "Parking",
+  },
+  {
+    id: 7,
+    name: "Water Bottle",
+  },
+  {
+    id: 8,
+    name: "Car Repair",
+  },
+  {
+    id: 9,
+    name: "Entrance Ticket",
+  },
+  {
     id: 3,
     name: "Other Expense",
   },
